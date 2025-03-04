bc26cdfaf69707216e47c4f1030a7c756cf7bd76
huangj1e <huangj1e@DESKTOP-3DGO7S7>
2025-03-04 11:03:18 +0800

    添加网站状态监控 页面

---
+++ WebAdmin/ViewModels/WebStatusViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebAdmin.ViewModels
+{
+	public class WebStatusViewModel : BindableBase
+	{
+        public WebStatusViewModel()
+        {
+
+        }
+	}
+}
---
+++ WebAdmin/Views/WebStatusView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WebAdmin.Views
+{
+    /// <summary>
+    /// Interaction logic for WebStatusView
+    /// </summary>
+    public partial class WebStatusView : UserControl
+    {
+        public WebStatusView()
+        {
+            InitializeComponent();
+        }
+    }
+}
