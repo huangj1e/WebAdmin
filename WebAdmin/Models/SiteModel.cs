b5b6354cf7d05bc3709f902bbd957ece37175ba1
huangj1e <huangj1e@DESKTOP-3DGO7S7>
2025-03-04 15:08:31 +0800

    添加 绑定继承

--- WebAdmin/Models/SiteModel.cs
+++ WebAdmin/Models/SiteModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace WebAdmin.Models;
 
-public class SiteModel
+public class SiteModel : BindableBase
 {
     [Key]
     [Display(Name = "Site ID")]
