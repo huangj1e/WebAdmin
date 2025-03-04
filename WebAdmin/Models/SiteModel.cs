81595b05ce30c656d686ade0f91c0616e2fdd8fc
huangj1e <huangj1e@DESKTOP-3DGO7S7>
2025-03-04 14:30:50 +0800

    添加 SiteModel 类

---
+++ WebAdmin/Models/SiteModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAdmin.Models;
+
+public class SiteModel
+{
+    [Key]
+    [Display(Name = "Site ID")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+
+    public string Address { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    [Display(Name = "Scan Interval")]
+    public DateTime LastScanTime { get; set; }
+
+    public bool Status { get; set; }
+}
