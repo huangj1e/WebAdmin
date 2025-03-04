924b77a24491a9604c24cfc2c24192f12365130b
huangj1e <huangj1e@DESKTOP-3DGO7S7>
2025-03-04 10:56:50 +0800

    添加 核心网站状态查询函数

---
+++ WebAdmin/Units.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAdmin;
+public static class Units
+{
+    public static async Task<bool> IsWebsiteUp(string url)
+    {
+        using HttpClient client = new();
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+            return response.IsSuccessStatusCode; // 2xx 视为网站正常
+        }
+        catch
+        {
+            return false; // 请求失败，网站可能不可用
+        }
+    }
+}
